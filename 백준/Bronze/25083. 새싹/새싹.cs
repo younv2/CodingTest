27b7b7b37e27fd7bb24d8536d0ceab75b664b7dd
younv2 <59247452+younv2@users.noreply.github.com>
2024-10-30 13:51:02 +0900

    [Bronze V] Title: 새싹, Time: 48 ms, Memory: 5308 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,14 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("         ,r'\"7");
+        Console.WriteLine("r`-_   ,'  ,/");
+        Console.WriteLine(" \\. \". L_r'");
+        Console.WriteLine("   `~\\/");
+        Console.WriteLine("      |");
+        Console.WriteLine("      |");
+    }
+}
