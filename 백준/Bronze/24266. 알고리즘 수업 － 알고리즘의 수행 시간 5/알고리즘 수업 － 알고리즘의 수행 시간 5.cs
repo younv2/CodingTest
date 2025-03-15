798afbaf8ec038cb88430d1063305ccda550bc91
younv2 <59247452+younv2@users.noreply.github.com>
2025-03-15 18:50:05 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 52 ms, Memory: 5496 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+class Program
+{
+    static void Main()
+    {
+        long num = long.Parse(Console.ReadLine());
+        Console.WriteLine(num*num*num);
+        Console.WriteLine(3);
+    }
+}
