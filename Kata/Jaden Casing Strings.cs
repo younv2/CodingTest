5c4d140c81adde463652cca2821a267b4e49d883
younv2 <younv2@naver.com>
2025-04-11 16:44:35 +0900

    [7Kyu] Title : Jaden Casing Strings

---
+++ Kata/Jaden Casing Strings.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+public static class JadenCase
+{
+    public static string ToJadenCase(this string phrase)
+    {
+        StringBuilder stringBuilder = new StringBuilder();
+        string[] t = phrase.Split(' ');
+        for(int i = 0; i < t.Length;i++)
+        {
+            t[i] = UpperFirstChar(t[i]);
+            stringBuilder.Append(t[i]+ " ");
+        }
+        stringBuilder.Remove(phrase.Length, 1);
+        return stringBuilder.ToString();
+    }
+    public static string UpperFirstChar(string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return null;
+        }
+
+        return char.ToUpper(input[0]) + input.Substring(1);
+    }
+}
